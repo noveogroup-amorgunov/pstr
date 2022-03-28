a15fcf91de1a2cc80a64e5d836c0b29a4e165a8a
amorgunov <amorgunov@samokat.ru>
2022-03-28 19:01:07 +0300

    Add test env to functions/deploy

--- .netlify/functions/publish.ts
+++ .netlify/functions/publish.ts
@@ -1,9 +1,15 @@
 import { Handler } from '@netlify/functions';
 
 const handler: Handler = async (event, context) => {
+
+  console.log(process.env.TELEGRAM_TESTING_TOKEN);
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
+    body: JSON.stringify({
+      message: "Hello World",
+      testEnvVar: process.env.TELEGRAM_TESTING_TOKEN,
+    }),
   };
 };
 
