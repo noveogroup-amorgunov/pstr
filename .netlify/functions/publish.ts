faad03fed179b08569fbdb30addb671a63c36f76
amorgunov <amorgunov@samokat.ru>
2022-03-28 19:14:21 +0300

    Try events

--- .netlify/functions/publish.ts
+++ .netlify/functions/publish.ts
@@ -1,14 +1,23 @@
 import { Handler } from '@netlify/functions';
+import axios from 'axios';
 
 const handler: Handler = async (event, context) => {
 
-  console.log(process.env.TELEGRAM_TESTING_TOKEN);
+  console.log(event.queryStringParameters);
+  console.log(event.body);
+
+  // const response = await axios.post(
+  //   `https://api.telegram.org/bot${TOKEN}/getMe`,
+  //   {}
+  // );
 
   return {
     statusCode: 200,
     body: JSON.stringify({
       message: "Hello World",
-      testEnvVar: process.env.TELEGRAM_TESTING_TOKEN,
+      body: event.body,
+      query: event.queryStringParameters,
+      // testEnvVar: response.data,
     }),
   };
 };
