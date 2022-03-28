8e903e69d3d5a447bcfb9a0dbe9f1a21ea3aeaf8
amorgunov <amorgunov@samokat.ru>
2022-03-28 19:22:23 +0300

    Try send review from function

--- .netlify/functions/publish.ts
+++ .netlify/functions/publish.ts
@@ -2,24 +2,24 @@ import { Handler } from '@netlify/functions';
 import axios from 'axios';
 
 const handler: Handler = async (event, context) => {
+  try {
+    const {token, chat_id} = JSON.parse(event.body);
 
-  console.log(event.queryStringParameters);
-  console.log(event.body);
-
-  // const response = await axios.post(
-  //   `https://api.telegram.org/bot${TOKEN}/getMe`,
-  //   {}
-  // );
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Hello World",
-      body: event.body,
-      query: event.queryStringParameters,
-      // testEnvVar: response.data,
-    }),
-  };
+      const response = await axios.post(
+        `https://api.telegram.org/bot${token}/getMe`,
+        {}
+      );
+    return {
+      statusCode: 200,
+      body: JSON.stringify({success: true, response: response.data}),
+    };
+  } catch (err) {
+    console.error(err);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({}),
+    }
+  }
 };
 
 export { handler };
